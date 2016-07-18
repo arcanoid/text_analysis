8a79f373127ddb690c78bd46848fe75e5233cbdb
Vasilis Kalligas <billkall@gmail.com>
2016-07-19 00:30:39 +0300

    Added proper spec

--- spec/text_analysis_spec.rb
+++ spec/text_analysis_spec.rb
@@ -5,7 +5,9 @@ describe TextAnalysis do
     expect(TextAnalysis::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'properly analysis input text' do
+    expect(subject.analyze_text "The brown fox jumps over the lazy dog").to eq( { :total_characters => 37,
+                                                                                  :total_characters_without_whitespaces => 30,
+                                                                                  :total_words => 8  } )
   end
 end
