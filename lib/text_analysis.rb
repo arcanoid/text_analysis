bd82bdd5d349c60cbeae7f23bb053f62c97750df
Vasilis Kalligas <billkall@gmail.com>
2016-07-22 00:23:08 +0300

    Added some more outcome regarding stop and common words found. Refs #3, closes #2

--- lib/text_analysis.rb
+++ lib/text_analysis.rb
@@ -4,9 +4,27 @@ module TextAnalysis
   def self.analyze_text(text_input)
     hash = {}
 
+    stop_words = File.readlines('data/stop_words/en.txt').map { |word| word.gsub("\n", '') }.inspect
+    input_words = text_input.split(/[\s]+/)
+
     hash[:total_characters] = text_input.length
     hash[:total_characters_without_whitespaces] = text_input.gsub(/\s+/, "").length
-    hash[:total_words] = text_input.split(/[\w-]+/).size
+    hash[:total_words] = input_words.size
+    hash[:stop_words_found] = input_words.select { |word| stop_words.include? word.downcase }.uniq
+    hash[:most_common_words] =
+        input_words.
+            group_by { |word| word.downcase }.
+            map { |k,v| { :word => k, :occurences => v.size } }.
+            sort_by { |hash| hash[:occurences] }.
+            reverse
+
+    hash[:most_common_non_stop_words] =
+        input_words.
+            reject { |word| stop_words.include? word.downcase }.
+            group_by { |word| word.downcase }.
+            map { |k,v| { :word => k, :occurences => v.size } }.
+            sort_by { |hash| hash[:occurences] }.
+            reverse
 
     hash
   end
--- spec/text_analysis_spec.rb
+++ spec/text_analysis_spec.rb
@@ -8,6 +8,20 @@ describe TextAnalysis do
   it 'properly analysis input text' do
     expect(subject.analyze_text "The brown fox jumps over the lazy dog").to eq( { :total_characters => 37,
                                                                                   :total_characters_without_whitespaces => 30,
-                                                                                  :total_words => 8  } )
+                                                                                  :total_words => 8,
+                                                                                  :stop_words_found => ["The", "over", "the"],
+                                                                                  :most_common_words => [ { :word => "the", :occurences => 2 },
+                                                                                                          { :word => "dog", :occurences => 1 },
+                                                                                                          { :word => "lazy", :occurences => 1 },
+                                                                                                          { :word => "over", :occurences => 1 },
+                                                                                                          { :word => "jumps", :occurences => 1 },
+                                                                                                          { :word => "fox", :occurences => 1 },
+                                                                                                          { :word => "brown", :occurences => 1 } ],
+                                                                                  :most_common_non_stop_words => [ { :word => "dog", :occurences => 1 },
+                                                                                                                   { :word => "lazy", :occurences => 1 },
+                                                                                                                   { :word => "jumps", :occurences => 1 },
+                                                                                                                   { :word => "fox", :occurences => 1 },
+                                                                                                                   { :word => "brown", :occurences => 1 } ]
+                                                                                } )
   end
 end
