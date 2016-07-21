fa30aaba74b7d1a19f1ab8f137f15da846712b60
Vasilis Kalligas <billkall@gmail.com>
2016-07-22 00:52:53 +0300

    Refactored code so as to have proper models and attributes instead of hash.

--- spec/text_analysis_spec.rb
+++ spec/text_analysis_spec.rb
@@ -5,23 +5,34 @@ describe TextAnalysis do
     expect(TextAnalysis::VERSION).not_to be nil
   end
 
-  it 'properly analysis input text' do
-    expect(subject.analyze_text "The brown fox jumps over the lazy dog").to eq( { :total_characters => 37,
-                                                                                  :total_characters_without_whitespaces => 30,
-                                                                                  :total_words => 8,
-                                                                                  :stop_words_found => ["The", "over", "the"],
-                                                                                  :most_common_words => [ { :word => "the", :occurences => 2 },
-                                                                                                          { :word => "dog", :occurences => 1 },
-                                                                                                          { :word => "lazy", :occurences => 1 },
-                                                                                                          { :word => "over", :occurences => 1 },
-                                                                                                          { :word => "jumps", :occurences => 1 },
-                                                                                                          { :word => "fox", :occurences => 1 },
-                                                                                                          { :word => "brown", :occurences => 1 } ],
-                                                                                  :most_common_non_stop_words => [ { :word => "dog", :occurences => 1 },
-                                                                                                                   { :word => "lazy", :occurences => 1 },
-                                                                                                                   { :word => "jumps", :occurences => 1 },
-                                                                                                                   { :word => "fox", :occurences => 1 },
-                                                                                                                   { :word => "brown", :occurences => 1 } ]
-                                                                                } )
+  context 'properly analysis input text' do
+    subject { TextAnalysis.analyze_text "The brown fox jumps over the lazy dog" }
+
+    it { should be_an_instance_of TextAnalysis::Result }
+    its(:total_characters) { should == 37 }
+    its(:total_characters_without_whitespaces) { should == 30 }
+    its(:total_words) { should == 8 }
+    its(:stop_words_found) { should == ['The', 'over', 'the'] }
+    its(:most_common_words) { should == [ { :word => 'the', :occurences => 2 },
+                                          { :word => 'dog', :occurences => 1 },
+                                          { :word => 'lazy', :occurences => 1 },
+                                          { :word => 'over', :occurences => 1 },
+                                          { :word => 'jumps', :occurences => 1 },
+                                          { :word => 'fox', :occurences => 1 },
+                                          { :word => 'brown', :occurences => 1 } ] }
+    its(:most_common_non_stop_words) { should == [ { :word => 'dog', :occurences => 1 },
+                                                   { :word => 'lazy', :occurences => 1 },
+                                                   { :word => 'jumps', :occurences => 1 },
+                                                   { :word => 'fox', :occurences => 1 },
+                                                   { :word => 'brown', :occurences => 1 } ] }
+  end
+
+  describe TextAnalysis::Result do
+    it { should respond_to :total_characters, :total_characters= }
+    it { should respond_to :total_characters_without_whitespaces, :total_characters_without_whitespaces= }
+    it { should respond_to :total_words, :total_words= }
+    it { should respond_to :stop_words_found, :stop_words_found= }
+    it { should respond_to :most_common_words, :most_common_words= }
+    it { should respond_to :most_common_non_stop_words, :most_common_non_stop_words= }
   end
 end
